655aa092b708aaab9637216212240dbd12f4b59f
Felipe Toro <yofelipetoro@gmail.com>
2023-01-30 11:33:21 -0500

    intecciones - final

--- js/app.js
+++ js/app.js
@@ -74,14 +74,3 @@ root.style.setProperty("--marquee-elements", marqueeContent.children.length);
 for(let i=0; i<marqueeElementsDisplayed; i++) {
   marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
 }
-
-//======================================
-//LOCOMOTIVE
-//======================================
-const scroll = new LocomotiveScroll({
-  el: document.querySelector('[data-scroll-container]'),
-  smooth: true,
-  tablet: { smooth: true },
-  smartphone: { smooth: true },
-  multiplier: 1
-});
